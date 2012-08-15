5a50f03b19f727cd077d6de1599f4fb1f06e1a71
Marc Sladek <marc.sladek@synventis.com>
2012-08-15 16:49:00 +0200

    getRandomImages() moved from WebUtils.java to ImageService.java clements-core dependency changed to version 1.29-SNAPSHOT due to ImageService.getRandomImages()

--- src/main/java/com/celements/photo/service/ImageService.java
+++ src/main/java/com/celements/photo/service/ImageService.java
@@ -1,6 +1,11 @@
 package com.celements.photo.service;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -15,10 +20,15 @@ import org.xwiki.model.reference.WikiReference;
 
 import com.celements.photo.container.ImageDimensions;
 import com.celements.photo.image.GenerateThumbnail;
+import com.celements.web.service.IWebUtilsService;
+import com.celements.web.utils.WebUtils;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.api.Attachment;
+import com.xpn.xwiki.api.Document;
 import com.xpn.xwiki.doc.XWikiAttachment;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.web.Utils;
 
 @Component
 public class ImageService implements IImageService {
@@ -74,6 +84,54 @@ public class ImageService implements IImageService {
     ImageDimensions imageDimensions = genThumbnail.getImageDimensions(imageInStream);
     imageInStream.reset();
     return imageDimensions;
+  }
+  
+  /**
+   * getRandomImages computes a set of <num> randomly chosen images
+   * from the given AttachmentList. It chooses the Images without dubilcats if
+   * possible.
+   */
+  public List<Attachment> getRandomImages(DocumentReference galleryRef, int num) {
+    try {
+      Document imgDoc = getContext().getWiki().getDocument(galleryRef, getContext()
+          ).newDocument(getContext());
+      List<Attachment> allImagesList = getWebUtilsService().getAttachmentListSorted(imgDoc,
+          "AttachmentAscendingNameComparator", true);
+      if (allImagesList.size() > 0) {
+        List<Attachment> preSetImgList = prepareMaxCoverSet(num, allImagesList);
+        List<Attachment> imgList = new ArrayList<Attachment>(num);
+        Random rand = new Random();
+        for (int i=1; i<=num ; i++) {
+          int nextimg = rand.nextInt(preSetImgList.size());
+          imgList.add(preSetImgList.remove(nextimg));
+        }
+        return imgList;
+      }
+    } catch (XWikiException e) {
+      LOGGER.error(e);
+    }
+    return Collections.emptyList();
+  }
+  
+  <T> List<T> prepareMaxCoverSet(int num, List<T> allImagesList) {
+    List<T> preSetImgList = new Vector<T>(num);
+    preSetImgList.addAll(allImagesList);
+    for(int i=2; i <= coveredQuotient(allImagesList.size(), num); i++) {
+      preSetImgList.addAll(allImagesList);
+    }
+    return preSetImgList;
+  }
+  
+  int coveredQuotient(int divisor, int dividend) {
+    if (dividend >= 0) {
+      return ( (dividend + divisor - 1) / divisor);
+    } else {
+      return (dividend / divisor);
+    }
+  }
+  
+  private IWebUtilsService getWebUtilsService() {
+    return Utils.getComponent(IWebUtilsService.class);
   }
 
 }
