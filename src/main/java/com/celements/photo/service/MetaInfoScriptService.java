6369cec7e849a46965089dc2606d3c823f8b6f69
ebeutler <edoardo.beutler@synventis.com>
2012-10-16 16:54:05 +0200

    initial commit

---
+++ src/main/java/com/celements/photo/service/MetaInfoScriptService.java
@@ -0,0 +1,41 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.celements.photo.service;
+
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.script.service.ScriptService;
+
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.Tag;
+
+@Component("celementsphoto")
+public class MetaInfoScriptService implements ScriptService {
+//  public List<Tag> getDirectoryTagsAsTagArray(InputStream imageFile, Class directory) throws MetadataException
+  
+  
+//  public Hashtable<String, String> getAllTags(InputStream imageFile) throws MetadataException
+  
+  
+  
+}
