90ef95572cd6b7256b4caf4cdf572be062e70f00
ebeutler <edoardo.beutler@synventis.com>
2012-10-16 17:05:20 +0200

    add method outlines

--- src/main/java/com/celements/photo/service/MetaInfoScriptService.java
+++ src/main/java/com/celements/photo/service/MetaInfoScriptService.java
@@ -19,6 +19,7 @@
  */
 package com.celements.photo.service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Hashtable;
 import java.util.List;
@@ -26,16 +27,95 @@ import java.util.List;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.script.service.ScriptService;
 
+import com.celements.photo.container.Metadate;
 import com.drew.metadata.MetadataException;
 import com.drew.metadata.Tag;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 
-@Component("celementsphoto")
+@Component("celmetatags")
 public class MetaInfoScriptService implements ScriptService {
-//  public List<Tag> getDirectoryTagsAsTagArray(InputStream imageFile, Class directory) throws MetadataException
+  public List<Tag> getDirectoryTagsAsTagArray(InputStream imageFile, Class directory) {
+    return null;
+  }
   
+  public Hashtable<String, String> getAllTags(InputStream imageFile) {
+    return null;
+  }
+
+  /**
+   * Get a specified Metadate from an image.
+   * 
+   * @param doc XWikiDocument of the album.
+   * @param id Hashcode of the image.
+   * @param imageMethods Used to get the image.
+   * @param tag Name of the metatag to get.
+   * @param context XWikiContext
+   * @return The Metadate of the specified Tag.
+   * @throws XWikiException
+   * @throws MetadataException
+   * @throws IOException
+   */
+  public Metadate getTag(XWikiDocument doc, String id, String tag, XWikiContext context) {
+    return null;
+  }
   
-//  public Hashtable<String, String> getAllTags(InputStream imageFile) throws MetadataException
+  /**
+   * Returns an array of Metadate objects, excluding the tags starting with
+   * the specified condition. If the condition is an empty String or null the
+   * all metadata available is returned.
+   * 
+   * @param doc XWikiDocument of the album.
+   * @param id Hashcode of the image.
+   * @param conditionTag Tags starting with this String are excluded from the result List.
+   * @param imageMethods Used to get the image.
+   * @param context XWikiContext
+   * @return Array of Metadate objects.
+   * @throws XWikiException
+   * @throws MetadataException
+   * @throws IOException
+   */
+  public Metadate[] getMetadataWithCondition(XWikiDocument doc, String id, 
+      String conditionTag, XWikiContext context) {
+    return null;
+  }
   
+  /**
+   * Get all metainformation contained in a specified image.
+   * 
+   * @param doc XWikiDocument of the album.
+   * @param id Hashcode of the image.
+   * @param imageMethods Used to get the image.
+   * @param context XWikiContext
+   * @return A List containing all the metadata from the specified image.
+   * @throws XWikiException
+   * @throws MetadataException
+   * @throws IOException 
+   */
+  public List<BaseObject> getMetadataList(XWikiDocument doc, String id, 
+      XWikiContext context) {
+    return null;
+  }
   
+  /**
+   * Extract metadata from an image and write it to objects, attached to a 
+   * XWikiDocument.
+   * 
+   * @param doc XWikiDocument of the album.
+   * @param id Hashcode of the image.
+   * @param imageMethods Used to get the image.
+   * @param context XWikiContext
+   * @throws XWikiException
+   * @throws MetadataException
+   * @throws IOException
+   */
+  public void extractMetaToDoc(XWikiDocument doc, String id, XWikiContext context) {
+    
+  }
   
+  public Tag getMetaTag() {
+    return null;
+  }
 }
