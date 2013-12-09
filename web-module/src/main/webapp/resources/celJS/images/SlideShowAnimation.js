f5259195101f2dc0366e2d3dd3e787b2d003a40e
Fabian Pichler <fabian.pichler@synventis.com>
2013-12-09 01:02:37 +0100

    implement fade transition in SlideShowAnimation.js

--- web-module/src/main/webapp/resources/celJS/images/SlideShowAnimation.js
+++ web-module/src/main/webapp/resources/celJS/images/SlideShowAnimation.js
@@ -23,6 +23,8 @@ CELEMENTS.presentation.SlideShowAnimation = function(celSlideShowObj, timeout,
       _slideShowEffect : undefined,
       _timeout : undefined,
       _slideShowDelayedThread : undefined,
+      _changeContentWithAnimationBind : undefined,
+      _afterFinishTransitionBind : undefined,
       _debug : false,
 
       _init : function(celSlideShowObj, timeout, slideShowEffect) {
@@ -31,6 +33,7 @@ CELEMENTS.presentation.SlideShowAnimation = function(celSlideShowObj, timeout,
         _me._restartDelayBind = _me._restartDelay.bind(_me);
         _me._delayedNextBind = _me._delayedNext.bind(_me);
         _me._changeContentWithAnimationBind = _me._changeContentWithAnimation.bind(_me);
+        _me._afterFinishTransition = _me._afterFinishTransition.bind(_me);
         _me._slideShowEffect = slideShowEffect || 'none';
         _me._timeout = timeout || 9999;
       },
@@ -59,27 +62,55 @@ CELEMENTS.presentation.SlideShowAnimation = function(celSlideShowObj, timeout,
           window.clearTimeout(_me._slideShowDelayedThread);
           _me._slideShowDelayedThread = undefined;
         }
-        _me._celSlideShowObj.getHtmlContainer().stopObserving(
-            'cel_yuiOverlay:changeContent', _me._changeContentWithAnimationBind);
-        _me._celSlideShowObj.getHtmlContainer().observe('cel_yuiOverlay:changeContent',
-            _me._changeContentWithAnimation);
         _me._slideShowDelayedThread = _me._delayedNextBind.delay(_me._timeout);
       },
 
       _delayedNext : function() {
         var _me = this;
         _me._slideShowDelayedThread = undefined;
+        if (_me._slideShowEffect != 'none') {
+          _me._celSlideShowObj.getHtmlContainer().stopObserving(
+              'cel_yuiOverlay:changeContent', _me._changeContentWithAnimationBind);
+          _me._celSlideShowObj.getHtmlContainer().observe('cel_yuiOverlay:changeContent',
+              _me._changeContentWithAnimation);
+        }
         _me._celSlideShowObj._navObj.nextSlide();
       },
 
       _changeContentWithAnimation : function(event) {
         var _me = this;
+        _me._celSlideShowObj.getHtmlContainer().stopObserving(
+            'cel_yuiOverlay:changeContent', _me._changeContentWithAnimationBind);
         var memoObj = event.memo;
         if (_me._slideShowEffect != 'none') {
-          if ((typeof console != 'undefined') && (typeof console.warn != 'undefined')) {
-            console.warn('unsupported transition effect: ', _me._slideShowEffect, memoObj);
+          if (_me._slideShowEffect == 'fade') {
+            var oldSlide = memoObj.slides[0];
+            var newSlide = memoObj.slides[1];
+            new Effect.Parallel(
+                [
+                  new Effect.Appear(newSlide, { from : 0, to : 1, sync: true }),
+                  new Effect.Fade(oldSlide, { from : 1, to : 0, sync: true })
+                ],
+                {
+                  'duration' : 2.0,
+                  'slideShowAnimObj' : memoObj,
+                  'afterFinish' : _me._afterFinishTransitionBind
+                }
+            );
+            event.stop();
+          } else if ((typeof console != 'undefined')
+              && (typeof console.warn != 'undefined')) {
+            console.warn('unsupported transition effect: ', _me._slideShowEffect,
+                memoObj);
           }
         }
+      },
+
+      _afterFinishTransition : function(effect) {
+        var _me = this;
+//        var slideConfig = effect.options.slideShowAnimObj;
+        _me._celSlideShowObj.getHtmlContainer().fire(
+            'cel_slideShow:slideTransitionFinished');
       }
 
   };
