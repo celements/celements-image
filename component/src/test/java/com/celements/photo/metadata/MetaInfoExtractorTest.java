5b6f880858c608f2434583a5219502476a83c12d
ebeutler <edoardo.beutler@synventis.com>
2013-12-10 18:22:57 +0100

    add cleanup when reading meta tags to filter most control characters

---
+++ component/src/test/java/com/celements/photo/metadata/MetaInfoExtractorTest.java
@@ -0,0 +1,41 @@
+package com.celements.photo.metadata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.celements.photo.metadata.MetaInfoExtractor;
+
+public class MetaInfoExtractorTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testCleanCtrlChars_realLiveExmpl() {
+    assertEquals("%G", new MetaInfoExtractor().cleanCtrlChars("%G"));
+  }
+
+  @Test
+  public void testCleanCtrlChars_filter() {
+    char c = 0xFFFA;
+    assertEquals("", new MetaInfoExtractor().cleanCtrlChars(String.valueOf(c)));
+  }
+
+  @Test
+  public void testCleanCtrlChars_noFilterSimple() {
+    String text = "Hi there you 2!";
+    assertEquals(text, new MetaInfoExtractor().cleanCtrlChars(text));
+  }
+
+  //-> problem: tab und zeilenumbruch werden gefiltert
+  @Test
+  public void testCleanCtrlChars_noFilterComplex() {
+    String text = "lkdjhfsaadsf;\'][{}23934!@#$%^&*()))_+±§}|\":{}<?>/.,÷≥≤`«æ…“‘œ∑´®†" +
+        "¥¨ˆøπ“åß∂ƒ©˙ ∆˚¬…æ§¡™\r\n£¢∞§¶•ªº–≠\t";
+    assertEquals(text, new MetaInfoExtractor().cleanCtrlChars(text));
+  }
+
+}
