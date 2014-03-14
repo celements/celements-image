cf5c097c8a435e1bf312cfe5a44edb4c59e38c4f
ebeutler <edoardo.beutler@synventis.com>
2014-03-14 16:21:51 +0100

    add @ComponentRole annotation

--- component/src/main/java/com/celements/photo/service/IMetaInfoService.java
+++ component/src/main/java/com/celements/photo/service/IMetaInfoService.java
@@ -3,11 +3,13 @@ package com.celements.photo.service;
 import java.util.List;
 import java.util.Map;
 
+import org.xwiki.component.annotation.ComponentRole;
 import org.xwiki.model.reference.DocumentReference;
 
 import com.drew.metadata.Tag;
 import com.xpn.xwiki.api.Attachment;
 
+@ComponentRole
 public interface IMetaInfoService {
   public List<Tag> getDirectoryTagsAsTagList(DocumentReference docRef, String filename, 
       String directory);
