c10f629d9bb60c0fe47e9ac263ff2a1499dbc08b
ebeutler <edoardo.beutler@synventis.com>
2015-01-22 10:34:31 +0100

    wait for crop image to load before going on to fix Firefox bug

--- web-module/src/main/webapp/resources/celJS/images/cropClass.js
+++ web-module/src/main/webapp/resources/celJS/images/cropClass.js
@@ -137,24 +137,40 @@ var setCropImage = function() {
     var params = prevSrc.replace(/.*\?/g, '');
     prevSrc = prevSrc.replace(/\?.*/g, '');
     if(prevSrc != $('cropImage').src) {
+      $('cropImage').stopObserving('load', cropPreviewLoaded);
+      $('cropImage').observe('load', cropPreviewLoaded);
       releaseSelection(true);
       $('cropImage').src = prevSrc;
       $('cropImage').setStyle({ width : '', height : ''});
-      $('cropZoom').src = prevSrc;
-      $('cropPreview').src = prevSrc;
-      $('crop_fixRatio').checked = false;
-      mainImgW = undefined;
-      mainImgH = undefined;
-      setZoomSize();
-      $('cropX').value = params.replace(/((^|(.*[\?&]))cropX=(\d*)\D?.*)|.*/g, '$4');
-      $('cropY').value = params.replace(/((^|(.*[\?&]))cropY=(\d*)\D?.*)|.*/g, '$4');
-      $('cropWidth').value = params.replace(/((^|(.*[\?&]))cropW=(\d*)\D?.*)|.*/g, '$4');
-      $('cropHeight').value = params.replace(/((^|(.*[\?&]))cropH=(\d*)\D?.*)|.*/g, '$4');
-      manualSelectDelayed(false);
       return true;
     }
   }
   return false;
+};
+
+var cropPreviewLoaded = function(event) {
+  var prevSrc = $('previewImg').src;
+  $('cropZoom').src = prevSrc;
+  $('cropPreview').src = prevSrc;
+  $('crop_fixRatio').checked = false;
+  mainImgW = undefined;
+  mainImgH = undefined;
+  setZoomSize();
+  $('cropX').value = params.replace(/((^|(.*[\?&]))cropX=(\d*)\D?.*)|.*/g, '$4');
+  $('cropY').value = params.replace(/((^|(.*[\?&]))cropY=(\d*)\D?.*)|.*/g, '$4');
+  $('cropWidth').value = params.replace(/((^|(.*[\?&]))cropW=(\d*)\D?.*)|.*/g, '$4');
+  $('cropHeight').value = params.replace(/((^|(.*[\?&]))cropH=(\d*)\D?.*)|.*/g, '$4');
+  manualSelectDelayed(false);$('cropZoom').src = prevSrc;
+  $('cropPreview').src = prevSrc;
+  $('crop_fixRatio').checked = false;
+  mainImgW = undefined;
+  mainImgH = undefined;
+  setZoomSize();
+  $('cropX').value = params.replace(/((^|(.*[\?&]))cropX=(\d*)\D?.*)|.*/g, '$4');
+  $('cropY').value = params.replace(/((^|(.*[\?&]))cropY=(\d*)\D?.*)|.*/g, '$4');
+  $('cropWidth').value = params.replace(/((^|(.*[\?&]))cropW=(\d*)\D?.*)|.*/g, '$4');
+  $('cropHeight').value = params.replace(/((^|(.*[\?&]))cropH=(\d*)\D?.*)|.*/g, '$4');
+  manualSelectDelayed(false);
 };
 
 var sizeSet = false;
