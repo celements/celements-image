03ee00fc2f970491d22da677f69dbfa57dd88f78
ebeutler <edoardo.beutler@synventis.com>
2015-05-26 18:49:18 +0200

    implement part pageing

--- web-module/src/main/webapp/resources/celJS/images/imageGalleryPaging.js
+++ web-module/src/main/webapp/resources/celJS/images/imageGalleryPaging.js
@@ -1,11 +1,64 @@
 (function(window, undefined) {
   "use strict";
 
+  var swiper;
+  
   var initScroller = function() {
     if ($$('div.galleryViewScrollContainer').size() > 0) {
-      //TODO init Swiper -> copy from PZ oder CFI
-      //TODO init EndlessScroller -> copy from PZ 
+      $$('.swiper-container').each(function(container) {
+        var slides = container.down('ul');
+        if(slides) {
+          slides.addClassName('swiper-wrapper');
+          slides.select('li').each(function(slide) {
+            slide.addClassName('swiper-slide');
+          });
+
+          swiper = new Swiper(sponsorenSwiperContainer, {
+            mousewheelControl : true,
+            mode : 'vertical',
+            autoResize : false,
+            slidePerGroup : 1,
+            slidesPerView : 'auto'
+          });
+        }
+      });
+      
+      $$('.presentationList').each(function(presentation) {
+        var endlessScroller = new CELEMENTS.anim.EndlessScroll(presentation,
+            endlessLoadAction, {
+          'scrollEventName' : 'cel_gallery:scroll',
+          'overlap' : 600
+        });
+        scrollContainer.observe('celEndlessScroll:ScrollPosEvent', swiperScrollOverflow);
+      });
     }
+  };
+  
+  var endlessLoadAction = function(htmlElem, endlessScroller, callbackFN) {
+	  //TODO implement
+    if (_me._dataProvider.hasMore()) {
+      _me._getScrollSlideInner().insert({ 'bottom' : _me._loadingIndicator });
+      _me.getColumnElem().fire('proz:contentChanged', { 'column' : _me });
+      _me._dataProvider.loadNextData(_me._endlessLoadCallbackBind.curry(callbackFN));
+      
+      
+      
+      { 
+        galleryNavConf
+        offset
+      }
+      
+      
+      
+    }
+  };
+  
+  var swiperScrollOverflow = function(event) {
+      event.stop();
+      var param = event.memo;
+      //getWrapperTranslate returns negative values, Thus we must add these.
+      param.currentScrollOverflow = swiper.wrapper.getHeight() - swiper.height
+                                    + swiper.getWrapperTranslate();
   };
 
   $j(document).ready(function() {
