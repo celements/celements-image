d657adb7251bc87905a7a87c3ff844ae8ac7537f
Fabian Pichler <fabian.pichler@synventis.com>
2021-03-23 16:34:28 +0100

    skipp container resize

--- web-module/src/main/webapp/resources/celJS/images/imageSlideShow.js
+++ web-module/src/main/webapp/resources/celJS/images/imageSlideShow.js
@@ -779,9 +779,9 @@
 
       changeContainerSize: function (newMaxWidth, newMaxHeight) {
         const _me = this;
-        console.log('changeContainerSize: ', _me._htmlElemId, newMaxWidth, newMaxHeight);
-        _me._getImageSlideShowObj()._getCelSlideShowObj().changeContainerSize(newMaxWidth,
-          newMaxHeight);
+        console.log('changeContainerSize: SKIP!!', _me._htmlElemId, newMaxWidth, newMaxHeight);
+        // _me._getImageSlideShowObj()._getCelSlideShowObj().changeContainerSize(newMaxWidth,
+        //   newMaxHeight);
       },
 
       _removeSlideShowDimension: function () {
