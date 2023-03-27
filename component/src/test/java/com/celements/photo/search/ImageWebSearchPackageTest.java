3662ca79ab276642b6a70fe6fc477d5be19020ea
Marc Sladek <marc@sladek.dev>
2023-03-27 20:38:21 +0200

    fix tests

--- component/src/test/java/com/celements/photo/search/ImageWebSearchPackageTest.java
+++ component/src/test/java/com/celements/photo/search/ImageWebSearchPackageTest.java
@@ -1,5 +1,7 @@
 package com.celements.photo.search;
 
+import static com.celements.common.test.CelementsTestUtils.*;
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -13,6 +15,7 @@ import com.celements.search.lucene.query.LuceneDocType;
 import com.celements.search.web.packages.WebSearchPackage;
 import com.celements.web.classcollections.IOldCoreClassConfig;
 import com.google.common.base.Joiner;
+import com.xpn.xwiki.plugin.lucene.LucenePlugin;
 import com.xpn.xwiki.web.Utils;
 
 public class ImageWebSearchPackageTest extends AbstractComponentTest {
@@ -21,6 +24,10 @@ public class ImageWebSearchPackageTest extends AbstractComponentTest {
 
   @Before
   public void prepareTest() throws Exception {
+    LucenePlugin lucenePlugin = createMockAndAddToDefault(LucenePlugin.class);
+    expect(getWikiMock().getPlugin(eq("lucene"), same(getContext())))
+        .andReturn(lucenePlugin).anyTimes();
+    expect(lucenePlugin.getAnalyzer()).andReturn(null).anyTimes();
     webSearchPackage = (ImageWebSearchPackage) Utils.getComponent(WebSearchPackage.class,
         ImageWebSearchPackage.NAME);
   }
@@ -48,7 +55,9 @@ public class ImageWebSearchPackageTest extends AbstractComponentTest {
   @Test
   public void test_getQueryRestriction_empty() {
     String searchTerm = "";
+    replayDefault();
     IQueryRestriction restriction = webSearchPackage.getQueryRestriction(null, searchTerm);
+    verifyDefault();
     assertNotNull(restriction);
     assertEquals("", restriction.getQueryString());
   }
@@ -56,7 +65,9 @@ public class ImageWebSearchPackageTest extends AbstractComponentTest {
   @Test
   public void test_getQueryRestriction_text_exact() {
     String searchTerm = LuceneUtils.exactify("find me");
+    replayDefault();
     IQueryRestriction restriction = webSearchPackage.getQueryRestriction(null, searchTerm);
+    verifyDefault();
     assertNotNull(restriction);
     assertEquals(String.format("(XWiki.PhotoAlbumClass.title:(+%s) OR "
         + "XWiki.PhotoAlbumClass.description:(+%s))", searchTerm, searchTerm),
@@ -67,8 +78,10 @@ public class ImageWebSearchPackageTest extends AbstractComponentTest {
   public void test_getQueryRestriction_text_tokenized() {
     String searchTerm1 = "find";
     String searchTerm2 = "us";
+    replayDefault();
     IQueryRestriction restriction = webSearchPackage.getQueryRestriction(null, Joiner.on(' ').join(
         searchTerm1, searchTerm2));
+    verifyDefault();
     assertNotNull(restriction);
     assertEquals(String.format("(XWiki.PhotoAlbumClass.title:(+%s* +%s*) OR "
         + "XWiki.PhotoAlbumClass.description:(+%s* +%s*))", searchTerm1, searchTerm2, searchTerm1,
