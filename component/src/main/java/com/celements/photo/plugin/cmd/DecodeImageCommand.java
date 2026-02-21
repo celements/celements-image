9c44f929cfbd8913f5d1b2fe9966edb90c0ca930
Fabian Pichler <fabian.pichler@synventis.com>
2026-02-21 03:08:30 +0100

    switch to org.apache.commons.io.IOUtils

--- component/src/main/java/com/celements/photo/plugin/cmd/DecodeImageCommand.java
+++ component/src/main/java/com/celements/photo/plugin/cmd/DecodeImageCommand.java
@@ -48,6 +48,7 @@ import org.apache.commons.imaging.common.bytesource.ByteSourceInputStream;
 import org.apache.commons.imaging.formats.jpeg.JpegImageParser;
 import org.apache.commons.imaging.formats.jpeg.segments.Segment;
 import org.apache.commons.imaging.formats.jpeg.segments.UnknownSegment;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,213 +59,213 @@ import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiAttachment;
 
-import edu.emory.mathcs.util.io.IOUtils;
-
 public class DecodeImageCommand {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DecodeImageCommand.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DecodeImageCommand.class);
 
-  public static final int COLOR_TYPE_RGB = 1;
-  public static final int COLOR_TYPE_CMYK = 2;
-  public static final int COLOR_TYPE_YCCK = 3;
+    public static final int COLOR_TYPE_RGB = 1;
+    public static final int COLOR_TYPE_CMYK = 2;
+    public static final int COLOR_TYPE_YCCK = 3;
 
-  private int colorType = COLOR_TYPE_RGB;
-  private String cmykProfile = "ECI_Offset_2009/ISOcoated_v2_300_eci.icc";
+    private int colorType = COLOR_TYPE_RGB;
+    private String cmykProfile = "ECI_Offset_2009/ISOcoated_v2_300_eci.icc";
 
-  public CelImage readImage(XWikiAttachment att, XWikiContext context) throws ImageReadException,
-      XWikiException {
-    return readImage(att.getContentInputStream(context), att.getFilename(), att.getMimeType(
-        context));
-  }
+    public CelImage readImage(XWikiAttachment att, XWikiContext context) throws ImageReadException,
+            XWikiException {
+        return readImage(att.getContentInputStream(context), att.getFilename(), att.getMimeType(
+                context));
+    }
 
-  public CelImage readImage(InputStream imageStream, String filename, String mimeType)
-      throws ImageReadException, XWikiException {
-    CelImage celImage = new CelImage();
-    BufferedImage image = null;
-    ByteArrayOutputStream createMarkStreamHelper = null;
-    try {
-      if (imageStream.markSupported()) {
-        imageStream.mark(Integer.MAX_VALUE);
-      } else {
-        createMarkStreamHelper = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len;
-        while ((len = imageStream.read(buffer)) > -1) {
-          createMarkStreamHelper.write(buffer, 0, len);
-        }
-        createMarkStreamHelper.flush();
-        // NOTICE: ByteArrayInputStream supports mark and marks by default on position 0
-        imageStream = new ByteArrayInputStream(createMarkStreamHelper.toByteArray());
-      }
-      colorType = COLOR_TYPE_RGB;
-      boolean hasAdobeMarker = false;
-      ImageInputStream stream = ImageIO.createImageInputStream(imageStream);
-      Iterator<ImageReader> iter = ImageIO.getImageReaders(stream);
-      while (iter.hasNext()) {
-        ImageReader reader = iter.next();
-        reader.setInput(stream);
-        ICC_Profile profile = null;
+    public CelImage readImage(InputStream imageStream, String filename, String mimeType)
+            throws ImageReadException, XWikiException {
+        CelImage celImage = new CelImage();
+        BufferedImage image = null;
+        ByteArrayOutputStream createMarkStreamHelper = null;
         try {
-          try {
-            image = reader.read(0);
-          } catch (CMMException cmmExcp) {
-            imageStream.reset();
-            image = readUsingJAI(imageStream, mimeType);
-          }
-        } catch (IIOException iioExcp) {
-          colorType = COLOR_TYPE_CMYK;
-          imageStream.reset();
-          hasAdobeMarker = hasAdobeMarker(imageStream, filename);
-          imageStream.reset();
-          profile = Imaging.getICCProfile(imageStream, filename);
-          WritableRaster raster = (WritableRaster) reader.readRaster(0, null);
-          if (colorType == COLOR_TYPE_YCCK) {
-            convertYcckToCmyk(raster);
-          }
-          if (hasAdobeMarker) {
-            convertInvertedColors(raster);
-          }
-          image = convertCmykToRgb(raster, profile);
+            if (imageStream.markSupported()) {
+                imageStream.mark(Integer.MAX_VALUE);
+            } else {
+                createMarkStreamHelper = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = imageStream.read(buffer)) > -1) {
+                    createMarkStreamHelper.write(buffer, 0, len);
+                }
+                createMarkStreamHelper.flush();
+                // NOTICE: ByteArrayInputStream supports mark and marks by default on position 0
+                imageStream = new ByteArrayInputStream(createMarkStreamHelper.toByteArray());
+            }
+            colorType = COLOR_TYPE_RGB;
+            boolean hasAdobeMarker = false;
+            ImageInputStream stream = ImageIO.createImageInputStream(imageStream);
+            Iterator<ImageReader> iter = ImageIO.getImageReaders(stream);
+            while (iter.hasNext()) {
+                ImageReader reader = iter.next();
+                reader.setInput(stream);
+                ICC_Profile profile = null;
+                try {
+                    try {
+                        image = reader.read(0);
+                    } catch (CMMException cmmExcp) {
+                        imageStream.reset();
+                        image = readUsingJAI(imageStream, mimeType);
+                    }
+                } catch (IIOException iioExcp) {
+                    colorType = COLOR_TYPE_CMYK;
+                    imageStream.reset();
+                    hasAdobeMarker = hasAdobeMarker(imageStream, filename);
+                    imageStream.reset();
+                    profile = Imaging.getICCProfile(imageStream, filename);
+                    WritableRaster raster = (WritableRaster) reader.readRaster(0, null);
+                    if (colorType == COLOR_TYPE_YCCK) {
+                        convertYcckToCmyk(raster);
+                    }
+                    if (hasAdobeMarker) {
+                        convertInvertedColors(raster);
+                    }
+                    image = convertCmykToRgb(raster, profile);
+                }
+                if (image != null) {
+                    celImage.addImage(image, reader.getImageMetadata(0));
+                    break;
+                }
+            }
+        } catch (IOException ioe) {
+            LOGGER.info("Failed to read image [" + filename + "].", ioe);
+        } finally {
+            IOUtils.closeQuietly(imageStream);
         }
-        if (image != null) {
-          celImage.addImage(image, reader.getImageMetadata(0));
-          break;
+        if (celImage.isEmpty()) {
+            throw new ImageReadException("Failed to read image [" + filename + "].");
         }
-      }
-    } catch (IOException ioe) {
-      LOGGER.info("Failed to read image [" + filename + "].", ioe);
-    } finally {
-      IOUtils.closeQuietly(imageStream);
+        return celImage;
     }
-    if (celImage.isEmpty()) {
-      throw new ImageReadException("Failed to read image [" + filename + "].");
+
+    // Requires Java Advanced Imaging - used as fallback for certain jpeg files containing
+    // conflicting information on where the actual image data begins (JFIF != EXIF)
+    BufferedImage readUsingJAI(InputStream inputStream, String mimeType) throws IOException {
+        SeekableStream seekableStream = new FileCacheSeekableStream(inputStream);
+        ParameterBlock paramBlock = new ParameterBlock();
+        paramBlock.add(seekableStream);
+        return JAI.create(mimeType.replace("image/", ""), paramBlock).getAsBufferedImage();
     }
-    return celImage;
-  }
 
-  // Requires Java Advanced Imaging - used as fallback for certain jpeg files containing
-  // conflicting information on where the actual image data begins (JFIF != EXIF)
-  BufferedImage readUsingJAI(InputStream inputStream, String mimeType) throws IOException {
-    SeekableStream seekableStream = new FileCacheSeekableStream(inputStream);
-    ParameterBlock paramBlock = new ParameterBlock();
-    paramBlock.add(seekableStream);
-    return JAI.create(mimeType.replace("image/", ""), paramBlock).getAsBufferedImage();
-  }
-
-  boolean hasAdobeMarker(InputStream imgIn, String filename) throws IOException,
-      ImageReadException {
-    boolean hasAdobeMarker = true;
-    JpegImageParser parser = new JpegImageParser();
-    ByteSource byteSource = new ByteSourceInputStream(imgIn, filename);// new ByteSourceFile(file);
-    List<Segment> segments = parser.readSegments(byteSource, new int[] { 0xffee }, true);
-    if ((segments != null) && !segments.isEmpty()) {
-      UnknownSegment app14Segment = (UnknownSegment) segments.get(0);
-      byte[] data = app14Segment.getSegmentData();
-      if ((data.length >= 12) && (data[0] == 'A') && (data[1] == 'd') && (data[2] == 'o')
-          && (data[3] == 'b')
-          && (data[4] == 'e')) {
-        int transform = data[11] & 0xff;
-        if (transform == 2) {
-          colorType = COLOR_TYPE_YCCK;
+    boolean hasAdobeMarker(InputStream imgIn, String filename) throws IOException,
+            ImageReadException {
+        boolean hasAdobeMarker = true;
+        JpegImageParser parser = new JpegImageParser();
+        ByteSource byteSource = new ByteSourceInputStream(imgIn, filename);// new
+                                                                           // ByteSourceFile(file);
+        List<Segment> segments = parser.readSegments(byteSource, new int[] { 0xffee }, true);
+        if ((segments != null) && !segments.isEmpty()) {
+            UnknownSegment app14Segment = (UnknownSegment) segments.get(0);
+            byte[] data = app14Segment.getSegmentData();
+            if ((data.length >= 12) && (data[0] == 'A') && (data[1] == 'd') && (data[2] == 'o')
+                    && (data[3] == 'b')
+                    && (data[4] == 'e')) {
+                int transform = data[11] & 0xff;
+                if (transform == 2) {
+                    colorType = COLOR_TYPE_YCCK;
+                }
+            }
         }
-      }
+        return hasAdobeMarker;
     }
-    return hasAdobeMarker;
-  }
 
-  void convertYcckToCmyk(WritableRaster raster) {
-    int height = raster.getHeight();
-    int width = raster.getWidth();
-    int stride = width * 4;
-    int[] pixelRow = new int[stride];
-    for (int h = 0; h < height; h++) {
-      raster.getPixels(0, h, width, 1, pixelRow);
-      for (int x = 0; x < stride; x += 4) {
-        int y = pixelRow[x];
-        int cb = pixelRow[x + 1];
-        int cr = pixelRow[x + 2];
-        int c = (int) ((y + (1.402 * cr)) - 178.956);
-        int m = (int) ((y - (0.34414 * cb) - (0.71414 * cr)) + 135.95984);
-        y = (int) ((y + (1.772 * cb)) - 226.316);
-        if (c < 0) {
-          c = 0;
-        } else if (c > 255) {
-          c = 255;
+    void convertYcckToCmyk(WritableRaster raster) {
+        int height = raster.getHeight();
+        int width = raster.getWidth();
+        int stride = width * 4;
+        int[] pixelRow = new int[stride];
+        for (int h = 0; h < height; h++) {
+            raster.getPixels(0, h, width, 1, pixelRow);
+            for (int x = 0; x < stride; x += 4) {
+                int y = pixelRow[x];
+                int cb = pixelRow[x + 1];
+                int cr = pixelRow[x + 2];
+                int c = (int) ((y + (1.402 * cr)) - 178.956);
+                int m = (int) ((y - (0.34414 * cb) - (0.71414 * cr)) + 135.95984);
+                y = (int) ((y + (1.772 * cb)) - 226.316);
+                if (c < 0) {
+                    c = 0;
+                } else if (c > 255) {
+                    c = 255;
+                }
+                if (m < 0) {
+                    m = 0;
+                } else if (m > 255) {
+                    m = 255;
+                }
+                if (y < 0) {
+                    y = 0;
+                } else if (y > 255) {
+                    y = 255;
+                }
+                pixelRow[x] = 255 - c;
+                pixelRow[x + 1] = 255 - m;
+                pixelRow[x + 2] = 255 - y;
+            }
+            raster.setPixels(0, h, width, 1, pixelRow);
         }
-        if (m < 0) {
-          m = 0;
-        } else if (m > 255) {
-          m = 255;
+    }
+
+    void convertInvertedColors(WritableRaster raster) {
+        int height = raster.getHeight();
+        int width = raster.getWidth();
+        int stride = width * 4;
+        int[] pixelRow = new int[stride];
+        for (int h = 0; h < height; h++) {
+            raster.getPixels(0, h, width, 1, pixelRow);
+            for (int x = 0; x < stride; x++) {
+                pixelRow[x] = 255 - pixelRow[x];
+            }
+            raster.setPixels(0, h, width, 1, pixelRow);
         }
-        if (y < 0) {
-          y = 0;
-        } else if (y > 255) {
-          y = 255;
+    }
+
+    BufferedImage convertCmykToRgb(Raster cmykRaster, ICC_Profile cmykProfile) throws IOException {
+        cmykProfile = getICCProfile(cmykProfile);
+        ICC_ColorSpace cmykCS = new ICC_ColorSpace(cmykProfile);
+        BufferedImage rgbImage = new BufferedImage(cmykRaster.getWidth(), cmykRaster.getHeight(),
+                BufferedImage.TYPE_INT_RGB);
+        WritableRaster rgbRaster = rgbImage.getRaster();
+        ColorSpace rgbCS = rgbImage.getColorModel().getColorSpace();
+        ColorConvertOp cmykToRgb = new ColorConvertOp(cmykCS, rgbCS, null);
+        cmykToRgb.filter(cmykRaster, rgbRaster);
+        return rgbImage;
+    }
+
+    ICC_Profile getICCProfile(ICC_Profile cmykProfile) throws IOException {
+        if (cmykProfile == null) {
+            cmykProfile = ICC_Profile.getInstance(getClass().getClassLoader().getResourceAsStream(
+                    getCMYKProfile()));
         }
-        pixelRow[x] = 255 - c;
-        pixelRow[x + 1] = 255 - m;
-        pixelRow[x + 2] = 255 - y;
-      }
-      raster.setPixels(0, h, width, 1, pixelRow);
+        if (cmykProfile.getProfileClass() != ICC_Profile.CLASS_DISPLAY) {
+            // Need to clone entire profile, due to a JDK 7 bug
+            byte[] profileData = cmykProfile.getData();
+            if (profileData[ICC_Profile.icHdrRenderingIntent] == ICC_Profile.icPerceptual) {
+                intToBigEndian(ICC_Profile.icSigDisplayClass, profileData,
+                        ICC_Profile.icHdrDeviceClass); // Header
+                                                       // is
+                                                       // first
+                cmykProfile = ICC_Profile.getInstance(profileData);
+            }
+        }
+        return cmykProfile;
     }
-  }
 
-  void convertInvertedColors(WritableRaster raster) {
-    int height = raster.getHeight();
-    int width = raster.getWidth();
-    int stride = width * 4;
-    int[] pixelRow = new int[stride];
-    for (int h = 0; h < height; h++) {
-      raster.getPixels(0, h, width, 1, pixelRow);
-      for (int x = 0; x < stride; x++) {
-        pixelRow[x] = 255 - pixelRow[x];
-      }
-      raster.setPixels(0, h, width, 1, pixelRow);
+    void intToBigEndian(int value, byte[] array, int index) {
+        array[index] = (byte) (value >> 24);
+        array[index + 1] = (byte) (value >> 16);
+        array[index + 2] = (byte) (value >> 8);
+        array[index + 3] = (byte) (value);
     }
-  }
 
-  BufferedImage convertCmykToRgb(Raster cmykRaster, ICC_Profile cmykProfile) throws IOException {
-    cmykProfile = getICCProfile(cmykProfile);
-    ICC_ColorSpace cmykCS = new ICC_ColorSpace(cmykProfile);
-    BufferedImage rgbImage = new BufferedImage(cmykRaster.getWidth(), cmykRaster.getHeight(),
-        BufferedImage.TYPE_INT_RGB);
-    WritableRaster rgbRaster = rgbImage.getRaster();
-    ColorSpace rgbCS = rgbImage.getColorModel().getColorSpace();
-    ColorConvertOp cmykToRgb = new ColorConvertOp(cmykCS, rgbCS, null);
-    cmykToRgb.filter(cmykRaster, rgbRaster);
-    return rgbImage;
-  }
-
-  ICC_Profile getICCProfile(ICC_Profile cmykProfile) throws IOException {
-    if (cmykProfile == null) {
-      cmykProfile = ICC_Profile.getInstance(getClass().getClassLoader().getResourceAsStream(
-          getCMYKProfile()));
+    public String getCMYKProfile() {
+        return cmykProfile;
     }
-    if (cmykProfile.getProfileClass() != ICC_Profile.CLASS_DISPLAY) {
-      // Need to clone entire profile, due to a JDK 7 bug
-      byte[] profileData = cmykProfile.getData();
-      if (profileData[ICC_Profile.icHdrRenderingIntent] == ICC_Profile.icPerceptual) {
-        intToBigEndian(ICC_Profile.icSigDisplayClass, profileData, ICC_Profile.icHdrDeviceClass); // Header
-                                                                                                  // is
-                                                                                                  // first
-        cmykProfile = ICC_Profile.getInstance(profileData);
-      }
+
+    public void setCMYKProfile(String cmykProfile) {
+        this.cmykProfile = cmykProfile;
     }
-    return cmykProfile;
-  }
-
-  void intToBigEndian(int value, byte[] array, int index) {
-    array[index] = (byte) (value >> 24);
-    array[index + 1] = (byte) (value >> 16);
-    array[index + 2] = (byte) (value >> 8);
-    array[index + 3] = (byte) (value);
-  }
-
-  public String getCMYKProfile() {
-    return cmykProfile;
-  }
-
-  public void setCMYKProfile(String cmykProfile) {
-    this.cmykProfile = cmykProfile;
-  }
 }
